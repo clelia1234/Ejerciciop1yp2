5fc55d0503e920b30aebf1e7879f2b32b3f583b9
Clelia Valdes <asesorialegal.cleliavaldes@gmail.com>
2023-08-05 12:58:37 -0600

    final

--- js/index.js
+++ js/index.js
@@ -2,21 +2,63 @@ const nombreUI = document.querySelector("#nombre");
 const telefonoUI = document.querySelector("#telefono");
 const precioUI = document.querySelector("#precio");
 const enviarBTN = document.querySelector("#btn");
-const mostrar = document.querySelector("#mostrar");
+const mostrar = document.querySelector(".mostrar");
+const formulario = document.querySelector("#formulario");
 
-enviarBTN.addEventListener("click",agregar)
+enviarBTN.addEventListener("click", agregar);
+document.addEventListener("DOMContentLoaded", pintar);
 
-function agregar(event){
+function agregar(event) {
     event.preventDefault();
-    const objeto ={
+    const objeto = {
         nombre: nombreUI.value,
         telefono: telefonoUI.value,
-        precio: precioUI.value, 
+        precio: precioUI.value 
 
     }
     datos = JSON.parse(localStorage.getItem("valores"));
-    if (datos ==null){
+    if (datos == null){
         datos = []
     }
+
+    datos.push(objeto);
+    localStorage.setItem("valores", JSON.stringify(datos));
+
+  
+formulario.reset(); 
+  
+pintar()
+
 }
 
+function pintar(){
+    datos = JSON.parse(localStorage.getItem("valores"));
+    mostrar.innerHTML = datos
+    .map((dato, index) => {
+        return `
+        <div class="card">
+        <h3>${dato.nombre}</h3>
+        <h3>${dato.telefono}</h3>
+        <h3>${dato.precio}</h3>
+        <button onClick="editar(${index})">editar</button>
+        </div>
+        `;
+        })
+        .join("");
+}
+
+function editar(index){
+const nombreEdit = prompt("edita el nombre", datos[index].nombre)
+datos[index].nombre =nombreEdit;
+
+const telefonoEdit = prompt("edita el telefono", datos[index].telefono)
+datos[index].telefono =telefonoEdit;
+
+const precioEdit = prompt("edita el precio", datos[index].precio)
+datos[index].precio =precioEdit;
+
+localStorage.setItem("valores",JSON.stringify(datos))
+
+pintar()
+
+}
